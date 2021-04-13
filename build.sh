828c5da0bae58e249e6bbedc8190b6322f94aa09
grm34 <jerem.pardo@tutanota.com>
2021-04-13 12:34:31 +0200

    Updates shell formatting syntax

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #
-# ~matrix
+# Copyright (C) 2021 matrix - Gabriele Gristina
 
 MATRIX_RTL8187_PATCH="${PWD}/rtl8187-matrix.patch"
 KALI_INJECTION_PATCH="${PWD}/kali-wifi-injection.patch"
+BACKPORTS_URL="https://cdn.kernel.org/pub/linux/kernel/projects/backports/stable/v5.10.16/backports-5.10.16-1.tar.xz"
+INJECTION_PATCH_URL="https://gitlab.com/kalilinux/packages/linux/raw/kali/master/debian/patches/features/all/kali-wifi-injection.patch?inline=false"
+MATRIX_PATCH_URL="https://raw.githubusercontent.com/grm34/backports-rtl8187/master/rtl8187-matrix.patch"
 
 CLEAN=0
 BUILD=0
@@ -12,18 +15,18 @@ UNINSTALL=0
 
 OS_DIST=$(lsb_release -is 2>/dev/null)
 
-if [ -z ${OS_DIST} ]; then
+if [[ -z ${OS_DIST} ]]; then
 	echo "! Missing lsb_release ... probably wrong OS" 1>&2
 	exit 2
 fi
 
-if [ "${OS_DIST}" != "Kali" ]; then
-	if [ $EUID -eq 0 ]; then
+if [[ ${OS_DIST} != "Kali" ]]; then
+	if [[ $EUID -eq 0 ]]; then
 		echo "! This script must not be run as root" 1>&2
 		exit 1
 	fi
 else
-	if [ $EUID -ne 0 ]; then
+	if [[ $EUID -ne 0 ]]; then
 		echo "! This script must be run as root" 1>&2
 		exit 1
 	fi
@@ -38,7 +41,7 @@ function usage()
 		"\t-u\t Uninstall rtl8187 kernel wireless.\n"
 }
 
-if [ $# -eq 0 ]; then
+if [[ $# -eq 0 ]]; then
 	usage
 	exit 1
 fi
@@ -67,105 +70,103 @@ while getopts ":cbiu" opt; do
 	esac
 done
 
-if ([ ${INSTALL} -eq 1 ] && [ ${UNINSTALL} -eq 1 ]) ||
-   ([ ${BUILD} -eq 1 ] && [ ${UNINSTALL} -eq 1 ]); then
+if [[ (${INSTALL} -eq 1 && ${UNINSTALL} -eq 1) ||
+    	(${BUILD} -eq 1  && ${UNINSTALL} -eq 1) ]]; then
 	echo "! Invalid arguments."
 	usage
 	exit 1
 fi
 
-if [ ${CLEAN} -eq 1 ]; then
+if [[ ${CLEAN} -eq 1 ]]; then
 	rm -rf tmp backports.tar.xz
 fi
 
-if [ ${BUILD} -eq 1 ]; then
+if [[ ${BUILD} -eq 1 ]]; then
 
 	rm -rf tmp
 
-	if [ ! -f "backports.tar.xz" ]; then
-		wget -c https://cdn.kernel.org/pub/linux/kernel/projects/backports/stable/v5.8/backports-5.8-1.tar.xz -O backports.tar.xz
-
-		if [ $? -ne 0 ]; then
+	if [[ ! -f "backports.tar.xz" ]]; then
+		if ! wget -c ${BACKPORTS_URL} -O backports.tar.xz; then
 			echo "! Failed to download backports ..."
 			exit 1
 		fi
 	fi
 
-	if [ ! -f "${MATRIX_RTL8187_PATCH}" ]; then
-		wget https://raw.githubusercontent.com/matrix/backports-rtl8187/master/rtl8187-matrix.patch -O rtl8187-matrix.patch
-		if [ $? -ne 0 ]; then
+	if [[ ! -f ${MATRIX_RTL8187_PATCH} ]]; then
+		if ! wget ${MATRIX_PATCH_URL} -O rtl8187-matrix.patch; then
 			echo "! Failed to download rtl8187 matrix patch ..."
 			exit 1
 		fi
 	fi
 
-	if [ ! -f "${KALI_INJECTION_PATCH}" ]; then
-		wget 'https://gitlab.com/kalilinux/packages/linux/raw/kali/master/debian/patches/features/all/kali-wifi-injection.patch?inline=false' -O kali-wifi-injection.patch
-		if [ $? -ne 0 ]; then
+	if [[ ! -f ${KALI_INJECTION_PATCH} ]]; then
+		if ! wget "${INJECTION_PATCH_URL}" -O kali-wifi-injection.patch; then
 			echo "! Failed to download wifi injection kali patch ..."
 			exit 1
 		fi
 	fi
 
 	mkdir -p tmp/backports
-	tar xJf backports.tar.xz -C tmp/backports --strip-components=1
-	if [ $? -ne 0 ]; then
+	if ! tar xJf backports.tar.xz -C tmp/backports --strip-components=1; then
 		echo "! Failed to extrack backports ..."
 		rm -rf backports.tar.xz
 		exit 1
 	fi
 
-	cd tmp/backports && \
-		patch -p1 --dry-run < ${MATRIX_RTL8187_PATCH} && patch -p1 < ${MATRIX_RTL8187_PATCH} && \
-		patch -p1 --dry-run < ${KALI_INJECTION_PATCH} && patch -p1 < ${KALI_INJECTION_PATCH} && \
-		make defconfig-rtl8187 && make && cd - &> /dev/null
+	if ! (cd tmp/backports && \
+		patch -p1 --dry-run < "${MATRIX_RTL8187_PATCH}" && \
+		patch -p1 < "${MATRIX_RTL8187_PATCH}" && \
+		patch -p1 --dry-run < "${KALI_INJECTION_PATCH}" && \
+		patch -p1 < "${KALI_INJECTION_PATCH}" && \
+		make defconfig-rtl8187 && make); then
 
-	if [ $? -ne 0 ]; then
 		echo "! Failed to build rtl8187 wireless driver."
 		exit 1
 	fi
 
-	cd ..
+	echo -e "\n>> " \
+	    "Kernel have been built correctly for backports wireless drivers.\n" \
+		"   Run ./build.sh -i to install rtl8187 wireless driver.\n\n"
 fi
 
-if [ ${INSTALL} -eq 1 ]; then
+if [[ ${INSTALL} -eq 1 ]]; then
 
-	if [ ! -d "tmp/backports" ]; then
-		echo "! Failed to install rtl8187 wireless driver : build dir not found."
+	if [[ ! -d "tmp/backports" ]]; then
+		echo "! Failed to install rtl8187 wireless driver :" \
+			" build dir not found."
 		exit 1
 	fi
 
-	cd tmp/backports && sudo make modules_install
-
-	if [ $? -ne 0 ]; then
+	if ! (cd tmp/backports && sudo make modules_install); then
 		echo "! Failed to install rtl8187 wireless driver."
 		exit 1
 	fi
 
-	cd - &> /dev/null
-
-	echo -e "\n>> Backports wireless drivers have been installed correctly.\n" \
-		"  If you want skip reboot please remove the following modules (and also those who use them) : mac80211 cfg80211\n" \
-		"  If everything is ok you can replug your external wireless device to load the new rtl8187 driver.\n\n"
+	echo -e "\n>> " \
+	    "Backports wireless drivers have been installed correctly.\n" \
+		"   If you want skip reboot please remove the following modules " \
+		"(and also those who use them) : mac80211 cfg80211\n" \
+		"   If everything is ok you can replug your external wireless " \
+		"device to load the new rtl8187 driver.\n\n"
 fi
 
-if [ ${UNINSTALL} -eq 1 ]; then
+if [[ ${UNINSTALL} -eq 1 ]]; then
 
-	if [ ! -d "tmp/backports" ]; then
-		echo "! Failed to uninstall rtl8187 wireless driver : build dir not found."
+	if [[ ! -d "tmp/backports" ]]; then
+		echo "! Failed to uninstall rtl8187 wireless driver :" \
+			" build dir not found."
 		exit 1
 	fi
 
-	cd tmp/backports && sudo make uninstall
-
-	if [ $? -ne 0 ]; then
+	if ! (cd tmp/backports && sudo make uninstall); then
 		echo "! Failed to uninstall rtl8187 wireless driver."
 		exit 1
 	fi
 
-	cd - &> /dev/null
-
-	echo -e "\n>> Backports wireless drivers have been uninstalled correctly.\n" \
-		"  If you want skip reboot please remove the following modules (and also those who use them) : mac80211 cfg80211\n" \
-		"  If everything is ok you can replug your external wireless device to load the old rtl8187 driver.\n\n"
+	echo -e "\n>> " \
+		"Backports wireless drivers have been uninstalled correctly.\n" \
+		"   If you want skip reboot please remove the following modules" \
+		"(and also those who use them) : mac80211 cfg80211\n" \
+		"   If everything is ok you can replug your external wireless" \
+		"device to load the old rtl8187 driver.\n\n"
 fi
