a2259b8e0493e7754bf303d5a3fe797b36ee8502
xpz3 <tcaller111@gmail.com>
2021-06-05 04:47:31 +0530

    fix

--- build.sh
+++ build.sh
@@ -94,7 +94,9 @@ if [ ${BUILD} -eq 1 ]; then
 		rm -rf backports.tar.xz
 		exit 1
 	fi
-
+	#Fix freeze
+	sed -i "s\cat $^ ;\cat ${PWD}/tmp/backports/net/wireless/certs/sforshee.hex ;\g" tmp/backports/net/wireless/Makefile
+exit 1
 	cd tmp/backports && \
 		patch -p1 --dry-run < ${MATRIX_RTL8187_PATCH} && patch -p1 < ${MATRIX_RTL8187_PATCH} && \
 		patch -p1 --dry-run < ${KALI_INJECTION_PATCH} && patch -p1 < ${KALI_INJECTION_PATCH} && \
